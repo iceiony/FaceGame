1df2341adc1a3680f2b819dd2594631789a4ee34
adrian <iceiony@yahoo.com>
2013-01-27 04:59:05 +0200

    fix ambiguity between path and uploadPath

--- engine/folderWatch.js
+++ engine/folderWatch.js
@@ -1,28 +1,30 @@
 var child_process = require('child_process'),
+    assert = require('assert'),
     path = require('path'),
     fs = require('fs'),
     fileUtils = require('./fileUtils'),
     uploadPath,
     peekForProcess;
 
-peekForProcess = function(path){
-    fs.readdir(path, function (err, files) {
+peekForProcess = function(uploadPath){
+    fs.readdir(uploadPath, function (err, files) {
+        assert.equal(null,err);
         files.forEach(function (file) {
             console.log('Processing '+file);
             if (file.lastIndexOf(".jpg") >= 0 || file.lastIndexOf(".png") >= 0 || file.lastIndexOf(".gif") >= 0 ) {
-                fileUtils.processFile(path.join(path, file));
+                fileUtils.processFile(path.join(uploadPath, file));
             }
         })
     });
 };
 
 
-exports.monitor = function (path) {
-    return child_process.fork("./engine/folderWatch", [], {env: {isChildProcess: true, MonitorPath: path}});
+exports.monitor = function (uploadPath) {
+    return child_process.fork("./engine/folderWatch", [], {env: {isChildProcess: true, MonitorPath: uploadPath}});
 };
 
 if (typeof process.env.isChildProcess !== 'undefined' && process.env.isChildProcess) {
     uploadPath = process.env.MonitorPath;
-    peekForProcess(path);
-    setTimeout(peekForProcess,60*10*1000,path);
+    peekForProcess(uploadPath);
+    setTimeout(peekForProcess,60*10*1000,uploadPath);
 }
