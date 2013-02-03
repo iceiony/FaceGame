b9c9da048131a15e971b077c85eb4002afbd0ab5
adrian <iceiony@yahoo.com>
2013-02-03 00:44:33 +0000

    cannot share mongoServerConfig so pass settings around

--- app.js
+++ app.js
@@ -5,19 +5,20 @@
 
 var express = require('express')
     , app = express()
+    , MongoStore = require('connect-mongo')(express)
     , http = require('http')
     , path = require('path')
 
     , mongo = require('mongodb')
-    , mongoServerConfig = new mongo.Server("127.0.0.1", 27017)
+    , dbSettings = {host:"127.0.0.1", port:27017}
     , routes = {
         login: require('./routes/login').login,
         quiz: require('./routes/quiz').quiz,
-        voted: require('./routes/vote')(mongoServerConfig)
+        vote: require('./routes/vote')(dbSettings).vote
     }
     , folderWatch = require('./engine/folderWatch');
 
-folderWatch.monitor(path.join(__dirname, 'input'),{host:mongoServerConfig.host,port:mongoServerConfig.port});
+folderWatch.monitor(path.join(__dirname, 'input'), dbSettings);
 
 
 app.configure(function () {
@@ -29,10 +30,17 @@ app.configure(function () {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+
+    app.use(express.cookieParser());
+    app.use(express.session({
+                secret: 'rainbow dash',
+                store: new MongoStore({db:"FaceGame"})
+            }
+        ));
+
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: 'rainbow dash'}));
+
 });
 
 app.configure('development', function () {
@@ -41,7 +49,7 @@ app.configure('development', function () {
 
 app.all('/', routes.login);
 app.all('/', routes.quiz);
-app.all('/user/:user/vote/:voted', routes.quiz);
+app.all('/user/:user/vote/:voted', routes.vote);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
--- routes/vote.js
+++ routes/vote.js
@@ -1,21 +1,26 @@
 var mongo = require('mongodb'),
+    assert = require('assert'),
     userData,
 
     voteHandler =  function(req,res){
         var quizQuestion = req.session.quizQuestions[0];
 
         req.session.quizQuestions = req.session.quizQuestions.slice(1); //pop it off the queue
-        res.redirect("/quiz/"+req.params.user+"/");
 
         userData.update(
             {username: req.params.user },
             {$inc: {score: quizQuestion.points[req.params.voted]}},
-            {upsert: true, w: 1});
+            {upsert: true, w: 1},
+        function(err,callback)
+        {
+            assert.equal(null,err);
+            res.redirect("/quiz/"+req.params.user+"/");
+        });
     };
 
-module.exports = function(mongoServerConfig){
+module.exports = function(dbSettings){
     //initialise connection to mongo
-    new mongo.Db("FaceGame", mongoServerConfig, {w: 1})
+    new mongo.Db("FaceGame", new  mongo.Server(dbSettings.host,dbSettings.port), {w: 1})
         .open(function (error, client) {
             if (error) throw error;
             userData = new mongo.Collection(client, "UserData");
