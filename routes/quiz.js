0585dee611e14c08929d79b3c62ade6d88dab748
adrian <iceiony@yahoo.com>
2013-03-03 03:09:21 +0000

    quiz should use one new mongo connection per request

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,58 +3,73 @@
  */
 
 var assert = require ( 'assert' ),
-    quizEngine = require ( '../engine/quiz-engine' ),
+    engine = require ( '../engine/quiz-engine' ),
 
-    dbSettings = require ( '../util/settings' ).dbSettings,
-    mongo = require ( 'mongodb' ),
-    userData,
+    settings    = require ( '../util/settings' ).dbSettings,
+    MongoClient = require ( 'mongodb' ).MongoClient,
+    MongoServer = require ( 'mongodb' ).Server;
 
-    _respondWith = function ( req , res , quizQuestion ) {
-        var userName = req.params.user,
-            userLinks = [],
-            i,
-            length;
 
-        for ( i = 0, length = quizQuestion.options.length ; i < length ; i ++ ) {
-            userLinks.push ( {
-                //href "/user/ionita.adri/vote/Koala"
-                href : ["/user/", userName, "/vote/", quizQuestion.options[i]].join ( '' ) , text : quizQuestion.options[i]
+var _getScore = function ( userName , callback ) {
+        var mongoClient = new MongoClient ( new MongoServer ( settings.host , settings.port ) );
+
+        mongoClient.open (
+            function ( err , mongoClient ) {
+                var userData = mongoClient.db ( 'FaceGame' ).collection ( 'UserData' );
+
+                userData.findOne (
+                    {username : userName} ,
+                    function ( err , record ) {
+                        assert.equal ( null , err );
+
+                        mongoClient.close ();
+                        callback ( record ? record.score : 0 );
+                    } );
             } );
-        }
+
+    },
+    _makeResponse = function ( req , res , quizQuestion ) {
+        var userName = req.params.user,
+            userLinks = [];
+
+        quizQuestion.options.forEach (
+            function ( option ) {
+                userLinks.push (
+                    {
+                        //href "/user/ionita.adri/vote/Koala"
+                        href : ["/user/", userName, "/vote/", option].join ( '' ) ,
+                        text : option
+                    } );
+            } );
 
         if ( req.isJson ) {
             res.json ( 200 , {
-                imageSrc : "/images/" + quizQuestion.imageName ,
-                links    : userLinks
+                links    : userLinks,
+                imageSrc : "/images/" + quizQuestion.imageName
             } )
         }
         else {
-            userData.findOne ( {username : userName} , function ( err , record ) {
-                assert.equal ( null , err );
-                res.render ( 'quiz' , {
-                    title    : "FaceGame" ,
-                    imageSrc : "/images/" + quizQuestion.imageName ,
-                    score    : record ? record.score : 0 ,
-                    links    : userLinks} );
-            } );
+
+            _getScore ( userName ,
+                function ( score ) {
+                    res.render ( 'quiz' , {
+                        title    : "FaceGame" ,
+                        score    : score ,
+                        links    : userLinks,
+                        imageSrc : "/images/" + quizQuestion.imageName
+                    } );
+                } );
         }
     };
 
 exports.quiz = function ( req , res ) {
     if ( ! req.isJson && req.session.quizQuestions.length > 0 ) {
-        _respondWith ( req , res , req.session.quizQuestions[0] );
+        _makeResponse ( req , res , req.session.quizQuestions[0] );
     }
     else {
-        quizEngine.generateQuestion ( function ( err , quizQuestion ) {
+        engine.generateQuestion ( function ( err , quizQuestion ) {
             req.session.quizQuestions.push ( quizQuestion );
-            _respondWith ( req , res , quizQuestion );
+            _makeResponse ( req , res , quizQuestion );
         } );
     }
 };
-
-
-new mongo.Db ( "FaceGame" , new mongo.Server ( dbSettings.host , dbSettings.port ) , {w : 1} )
-    .open ( function ( error , client ) {
-        if ( error ) throw error;
-        userData = new mongo.Collection ( client , "UserData" );
-    } );
