d9e6dba5e8e0f8ddd3822245287f0f46964730e7
adrian <iceiony@yahoo.com>
2013-03-03 00:41:50 +0000

    re-factor some of the folder-watch

--- engine/folder-watch.js
+++ engine/folder-watch.js
@@ -1,32 +1,55 @@
-var child_process = require ( 'child_process' ),
-    runAsSeparateProcess;
+var child_process = require ( 'child_process' );
 
-runAsSeparateProcess = function () {
+var _runAsSeparateProcess = function () {
     var assert = require ( 'assert' ),
-        path = require ( 'path' ),
-        fs = require ( 'fs' ),
-        uploadPath = process.env.MonitorPath,
-        fp = require ( './file-processing' ),
+        path   = require ( 'path' ),
+        fs     = require ( 'fs' ),
+        fp     = require ( './file-processing' );
+
+    var uploadPath     = process.env.MonitorPath,
+        fileExtension  = ['jpg', 'png', 'gif'],
+
+        isImageFile    = function ( fileName ) {
+            var extension = Array.prototype.splice.call ( fileName , fileName.lastIndexOf ( '.' ) + 1 ).join ( '' );
+            return fileExtension.indexOf ( extension ) >= 0;
+        },
+
         peekForProcess = function ( uploadPath ) {
-            fs.readdir ( uploadPath , function ( err , files ) {
-                assert.equal ( null , err );
-                files.forEach ( function ( file ) {
-                    console.log ( 'Processing ' + file );
-                    if ( file.lastIndexOf ( ".jpg" ) >= 0 || file.lastIndexOf ( ".png" ) >= 0 || file.lastIndexOf ( ".gif" ) >= 0 ) {
-                        fp.processFile ( path.join ( uploadPath , file ) );
-                    }
+            fs.readdir ( uploadPath ,
+                function ( err , files ) {
+                    assert.equal ( null , err );
+
+                    files.forEach (
+                        function ( file ) {
+                            console.log ( 'Processing ' + file );
+                            if ( isImageFile ( file ) ) {
+                                fp.processFile ( path.join ( uploadPath , file ) );
+                            }
+                        } );
+
                 } );
-            } );
         };
 
     peekForProcess ( uploadPath );
+    //peek for  changes every 10 minutes
     setInterval ( peekForProcess , 60 * 10 * 1000 , uploadPath );
 };
 
 exports.monitor = function ( uploadPath ) {
-    return child_process.fork ( "./engine/folder-watch" , [] , {env : {isChildProcess : true , MonitorPath : uploadPath}} );
+    return child_process.fork (
+        "./engine/folder-watch" ,
+        [] ,
+        {
+            env : {
+                isChildProcess : true ,
+                MonitorPath    : uploadPath}
+        }
+    );
 };
 
-if ( typeof process.env.isChildProcess !== 'undefined' && process.env.isChildProcess ) {
-    runAsSeparateProcess ();
+if ( typeof process.env.isChildProcess !== 'undefined'
+    && process.env.isChildProcess ) {
+
+    _runAsSeparateProcess ();
+
 }
