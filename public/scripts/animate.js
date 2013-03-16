e0cd75f4646310aad4c1ea281089c82f860d50b7
adrian <iceiony@yahoo.com>
2013-03-16 02:19:48 +0000

    bubble animation made for browsers with no canvas

--- public/scripts/animate.js
+++ public/scripts/animate.js
@@ -5,7 +5,27 @@ Game.nameSpace ( "Game.Animate" );
         NEGATIVE_COLOR = '#FF5300';
 
 
-    var _moveParticles = function ( animate ) {
+    var _generateNewParticles = function ( animate , options ) {
+            var particle = new createjs.Text ( options.symbol , 'bold 2em Arial' , options.colour );
+
+            _.each ( _.range ( options.count ) ,
+                function ( ordinal ) {
+                    var clone = particle.clone ();
+
+                    clone.x = Math.random () * ( animate.stageWidth - 20)
+                    clone.y = animate.stageHeight - ( 2 + Math.random () * 5 );
+                    clone.alpha = 0.2 + Math.random () * 0.8;
+                    clone.scaleX = clone.scaleY = 0.5 + Math.random () * 1.5;
+
+                    //custom property
+                    clone.speed = 0.5 + Math.random ();
+
+                    animate.particles.push ( clone );
+                    animate.stage.addChild ( clone );
+                }
+            );
+        },
+        _moveParticles = function ( animate ) {
             var firstPositive = 0;
 
             _.each ( animate.particles ,
@@ -57,79 +77,75 @@ Game.nameSpace ( "Game.Animate" );
                 function () {
                     bubble.remove ();
                 } );
-        };
+        },
+        _isCanvasSupported = (function () {
+            var elem = document.createElement ( 'canvas' );
+            return ! ! (elem.getContext && elem.getContext ( '2d' ));
+        } ());
 
 
-    Game.Animate = function ( canvasId ) {
+    Game.Animate = function ( canvasId , domId ) {
         var that = this,
             dom = $ ( '#' + canvasId ),
             scoreText = dom.text (),
             initialColor = scoreText.indexOf ( '-' ) < 0 ? POSITIVE_COLOR : NEGATIVE_COLOR;
 
-        this.stage = new createjs.Stage ( canvasId );
-
-        this.score = new createjs.Text ( scoreText , 'bold 2em Arial' , initialColor );
-        this.score.x = 8;
-        this.score.y = 34;
-        this.stage.addChild ( this.score );
-
-        this.particles = [];
-        this.stageWidth = dom.width ();
-        this.stageHeight = dom.height ();
-
-        createjs.Ticker.setFPS ( 46 );
-        createjs.Ticker.addEventListener ( 'tick' , function () {
-            that.stage.update ();
-        } );
-
-        //counts as internal clock, different from FPS ticks
-        setInterval ( function(){_moveParticles(that);} , 1000 / 60  );
-
-    };
-
-
-    Game.Animate.prototype.text = function ( text , lastValue , clickEvent ) {
-        var that = this,
-            particle = new createjs.Text ( '' , 'bold 2em Arial' );
-
-        this.score.text = text;
-        if ( lastValue > 0 ) {
-            particle.text = '+';
-            particle.color = POSITIVE_COLOR;
+        if ( ! _isCanvasSupported ) {
+             this.scoreBox  = $('#'+domId);
         }
         else {
-            particle.text = '-';
-            particle.color = NEGATIVE_COLOR;
+            this.stage = new createjs.Stage ( canvasId );
+
+            this.score = new createjs.Text ( scoreText , 'bold 2em Arial' , initialColor );
+            this.score.x = 8;
+            this.score.y = 34;
+            this.stage.addChild ( this.score );
+
+            this.particles = [];
+            this.stageWidth = dom.width ();
+            this.stageHeight = dom.height ();
+
+            createjs.Ticker.setFPS ( 46 );
+            createjs.Ticker.addEventListener ( 'tick' , function () {
+                that.stage.update ();
+            } );
+
+            //counts as internal clock, different from FPS ticks
+            setInterval ( function () {_moveParticles ( that );} , 1000 / 60 );
         }
 
-        _.each ( _.range ( Math.abs ( lastValue ) * 2 ) ,
-            function ( ordinal ) {
-                var clone = particle.clone ();
-
-                clone.x = 10 + Math.random () * ( that.stageWidth - 10)
-                clone.y = that.stageHeight - ( 2 + Math.random () * 5 );
-                clone.alpha = 0.2 + Math.random () * 0.8;
-                clone.scaleX = clone.scaleY = 0.5 + Math.random () * 1.5;
-
-                //custom property
-                clone.speed = 0.5 + Math.random ();
-
-                that.particles.push ( clone );
-                that.stage.addChild ( clone );
-            }
-        );
-
-        //blink text
-        that.score.color = that.score.text.indexOf ( '-' ) < 0 ? POSITIVE_COLOR : NEGATIVE_COLOR;
-        if ( lastValue < 0 ) _blinkText ( this );
-
-        //voteScore bubble next to mouse
-        _voteBuble ( lastValue , clickEvent );
     };
 
-    Game.Animate.isCanvasSupported = (function () {
-        var elem = document.createElement ( 'canvas' );
-        return ! ! (elem.getContext && elem.getContext ( '2d' ));
+
+    Game.Animate.prototype.text = (function () {
+        if ( ! _isCanvasSupported ) {
+
+            return function ( text , lastValue , clickEvent ) {
+                this.scoreBox.text(text);
+                _voteBuble ( lastValue , clickEvent );//score bubble next to mouse
+            };
+
+        }
+        else {
+
+            return function ( text , lastValue , clickEvent ) {
+                this.score.text = text;
+
+                _generateNewParticles ( this , {
+                    symbol : lastValue > 0 ? '+' : '-' ,
+                    colour : lastValue > 0 ? POSITIVE_COLOR : NEGATIVE_COLOR ,
+                    count  : Math.abs ( lastValue * 2 )
+                } );
+
+                //blink text
+                this.score.color = this.score.text.indexOf ( '-' ) < 0 ? POSITIVE_COLOR : NEGATIVE_COLOR;
+                if ( lastValue < 0 ) _blinkText ( this );
+
+                _voteBuble ( lastValue , clickEvent );//score bubble next to mouse
+            };
+
+        }
+
     } ());
 
 } ());
